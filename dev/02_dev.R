f6f705fa155c52e9c622b09ba6fa7fa8d05507da
cervan <cervan@thinkr.fr>
2023-04-05 13:57:33 +0200

    [feat] Add the new Referencial Class to read and check xlsForm
    tags : feat, chore, docs
    
    why :
    
    - Starting to develop the class relative to xlsform
    
    how
    
    - Using R6 ans somes utils
    
    issue #5

--- dev/02_dev.R
+++ dev/02_dev.R
@@ -1,17 +1,19 @@
 # Building a Prod-Ready, Robust Shiny Application.
-# 
-# README: each step of the dev files is optional, and you don't have to 
-# fill every dev scripts before getting started. 
-# 01_start.R should be filled at start. 
+#
+# README: each step of the dev files is optional, and you don't have to
+# fill every dev scripts before getting started.
+# 01_start.R should be filled at start.
 # 02_dev.R should be used to keep track of your development during the project.
 # 03_deploy.R should be used once you need to deploy your app.
-# 
-# 
+#
+#
 ###################################
 #### CURRENT FILE: DEV SCRIPT #####
 ###################################
 
 # Engineering
+
+## Update pkgdown
 
 ## Dependencies ----
 ## Add one line by package you want to add as dependency
@@ -24,7 +26,7 @@ golem::add_module( name = "name_of_module2" ) # Name of the module
 
 ## Add helper functions ----
 ## Creates fct_* and utils_*
-golem::add_fct( "helpers" ) 
+golem::add_fct( "helpers" )
 golem::add_utils( "helpers" )
 
 ## External resources
@@ -35,9 +37,9 @@ golem::add_css_file( "custom" )
 
 ## Add internal datasets ----
 ## If you have data in your package
-usethis::use_data_raw( name = "survey", open = FALSE ) 
-usethis::use_data_raw( name = "choices", open = FALSE ) 
-usethis::use_data_raw( name = "setting", open = FALSE ) 
+usethis::use_data_raw( name = "survey", open = FALSE )
+usethis::use_data_raw( name = "choices", open = FALSE )
+usethis::use_data_raw( name = "setting", open = FALSE )
 
 ## Tests ----
 ## Add one line by test you want to create
@@ -59,26 +61,26 @@ covrpage::covrpage()
 ## CI ----
 ## Use this part of the script if you need to set up a CI
 ## service for your application
-## 
+##
 ## (You'll need GitHub there)
 usethis::use_github()
 
 # GitHub Actions
-usethis::use_github_action() 
+usethis::use_github_action()
 # Chose one of the three
 # See https://usethis.r-lib.org/reference/use_github_action.html
-usethis::use_github_action_check_release() 
-usethis::use_github_action_check_standard() 
-usethis::use_github_action_check_full() 
+usethis::use_github_action_check_release()
+usethis::use_github_action_check_standard()
+usethis::use_github_action_check_full()
 # Add action for PR
 usethis::use_github_action_pr_commands()
 
 # Travis CI
-usethis::use_travis() 
-usethis::use_travis_badge() 
+usethis::use_travis()
+usethis::use_travis_badge()
 
-# AppVeyor 
-usethis::use_appveyor() 
+# AppVeyor
+usethis::use_appveyor()
 usethis::use_appveyor_badge()
 
 # Circle CI
