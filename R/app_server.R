7bb4bd5622d19663cff1ec5ff77257a5201d6e06
Edouard <edouard.legoupil@gmail.com>
2023-09-26 22:20:38 -0500

    rev

--- R/app_server.R
+++ R/app_server.R
@@ -7,9 +7,45 @@
 #' @noRd
 #' @keywords internal
 app_server <- function(input, output, session) {
+  
+  ## load the refrential
 
   ## add a reactive value object to pass by elements between objects
-  AppReactiveValue <-  reactiveValues()
+  AppReactiveValue <-  reactiveValues(
+    title = "Title for your data collection project",
+    year = "2023",
+    abstract="Provide an abstract for the data collection activities",
+    country = "PAN", 
+    coverage = "yes",
+    survey = readxl::read_xlsx(system.file("SurveyDesigner_Referential.xlsx",
+                                           package = "surveyDesigner"),
+                               sheet = "survey"),
+    choices = readxl::read_xlsx(system.file("SurveyDesigner_Referential.xlsx",
+                                           package = "surveyDesigner"),
+                               sheet = "choices"),
+    indicator = readxl::read_xlsx(system.file("SurveyDesigner_Referential.xlsx",
+                                            package = "surveyDesigner"),
+                                sheet = "indicator"),
+    indicator_survey = readxl::read_xlsx(system.file("SurveyDesigner_Referential.xlsx",
+                                            package = "surveyDesigner"),
+                                sheet = "indicator_survey"),
+    indicator_choices = readxl::read_xlsx(system.file("SurveyDesigner_Referential.xlsx",
+                                            package = "surveyDesigner"),
+                                sheet = "indicator_choices"),
+    indicator_population = readxl::read_xlsx(system.file("SurveyDesigner_Referential.xlsx",
+                                            package = "surveyDesigner"),
+                                sheet = "indicator_population"),
+    indicator_disaggregation = readxl::read_xlsx(system.file("SurveyDesigner_Referential.xlsx",
+                                            package = "surveyDesigner"),
+                                sheet = "indicator_disaggregation") ,
+    
+    ## get list of indicator topic
+    
+    
+    
+    
+    
+  )
   # pins::board_register() # connect to pin board if needed
   callModule(mod_home_server, "home_ui_1")
   callModule(mod_context_server, "context_ui_1", AppReactiveValue)
